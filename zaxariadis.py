a817d100bef9b9165b29fb7a45fbf1ca94efdd6a
GiannisVagionakis <57878676+GiannisVagionakis@users.noreply.github.com>
2021-01-07 12:33:16 +0200

    SN = 4.66 + 2-opt = 4.63 + rlfrommax = 4.58

--- zaxariadis.py
+++ zaxariadis.py
@@ -1,6 +1,8 @@
+import copy
 import math
+import pprint
 import random
-
+from Solution_Drawer import SolDrawer
 
 
 class Node:
@@ -38,13 +40,13 @@ class Model:
 
         self.dist_matrix = [[0 for j in range(0, len(self.all_nodes))] for k in range(0, len(self.all_nodes))]
         for i in range(0, len(self.all_nodes)):
-         for j in range(1, len(self.all_nodes)):
-          source = self.all_nodes[i]
-          target = self.all_nodes[j]
-          dx_2 = (source.x - target.x)**2
-          dy_2 = (source.y - target.y) ** 2
-          dist = round(math.sqrt(dx_2 + dy_2))
-          self.dist_matrix[i][j] = dist
+            for j in range(1, len(self.all_nodes)):
+              source = self.all_nodes[i]
+              target = self.all_nodes[j]
+              dx_2 = (source.x - target.x)**2
+              dy_2 = (source.y - target.y) ** 2
+              dist = round(math.sqrt(dx_2 + dy_2))
+              self.dist_matrix[i][j] = dist
         #dict with delivery time between nodes
         for i in range(0, len(self.all_nodes)):
             timetonodes = []
@@ -81,6 +83,12 @@ for node in all_nodes:
     prt_width(node.type)
     print('\n')
 """
+
+class Solution:
+    def __init__(self):
+        self.cost = 0.0
+        self.routes = []
+
 class Solver:
     def __init__(self, m):
         self.all_nodes = m.all_nodes
@@ -141,11 +149,11 @@ class Solver:
         convertCoordinates()
         findPolarAngle()
 
-
         def findNodeWithID(id):
             for node in self.all_nodes:
                 if node.id == id:
                     return node
+
         """
         pprint.pprint(polarsOfAngling)
         values=[]
@@ -263,23 +271,67 @@ class Solver:
             routepiecereversed.append(node)
         ri = 0
         for j in range(i, k):
-            route.sequenceOfNodes[j] = routepiecereversed[ri]
+            route.sequenceOfNodes[j] = copy.deepcopy(routepiecereversed[ri])
             ri += 1
         return route
 
+    def relocateFromMax(self, relroutes):
+        for i in range(0, 100):
+            maxroute = 0
+            maxvalue = 0
+            for i in range(0, len(relroutes)):
+                rt = relroutes[i]
+                if maxvalue < rt.time:
+                    maxroute = i
+                    maxvalue = rt.time
+            movereducal = 0
+            foundreplacement = False
+            for targetRouteIndex in range(0, len(relroutes)):
+                rt2 = relroutes[targetRouteIndex]
+                if rt2 == relroutes[maxroute]:
+                    continue
+                for originNodeIndex in range(1, len(relroutes[maxroute].sequenceOfNodes)):
+                    if relroutes[maxroute].sequenceOfNodes[originNodeIndex].demand + rt2.load > 3000:
+                        continue
+                    for targetNodeIndex in range(0, len(rt2.sequenceOfNodes)):
+                        A = relroutes[maxroute].sequenceOfNodes[originNodeIndex - 1]
+                        B = relroutes[maxroute].sequenceOfNodes[originNodeIndex]
+                        origincost = - self.dist_matrix[A.id][B.id]
+                        if originNodeIndex != len(relroutes[maxroute].sequenceOfNodes) - 1:
+                            C = relroutes[maxroute].sequenceOfNodes[originNodeIndex + 1]
+                            origincost = - self.dist_matrix[A.id][B.id] - self.dist_matrix[B.id][C.id] + self.dist_matrix[A.id][C.id]
+                        F = rt2.sequenceOfNodes[targetNodeIndex]
+                        targetcost = self.dist_matrix[F.id][B.id]
+                        if targetNodeIndex != len(rt2.sequenceOfNodes) - 1:
+                            G = rt2.sequenceOfNodes[targetNodeIndex + 1]
+                            targetcost = self.dist_matrix[F.id][B.id] + self.dist_matrix[B.id][G.id] - self.dist_matrix[F.id][G.id]
+                        if origincost < movereducal and rt2.time + targetcost < relroutes[maxroute].time:
+                            foundreplacement = not foundreplacement
+                            keeporiginnode = originNodeIndex
+                            keeptargetnode = targetNodeIndex
+                            targetrtindex = targetRouteIndex
+                            keeporigincost = origincost
+                            keeptargetcost = targetcost
+            if foundreplacement == True:
+                originroute = relroutes[maxroute]
+                targetroute = relroutes[targetrtindex]
+                originroute.time = originroute.time + keeporigincost
+                targetroute.time = targetroute.time + keeptargetcost
+                B = originroute.sequenceOfNodes[keeporiginnode]
+                originroute.load -= B.demand
+                targetroute.load += B.demand
+                del originroute.sequenceOfNodes[keeporiginnode]
+                targetroute.sequenceOfNodes.insert(keeptargetnode + 1, B)
+        return relroutes
 
-
-
-
-
-
-    def twoOpt(self):
-        tryingroutes = sol.copy()
+    def twoOpt(self, routesgiven):
+        tryingroutes = routesgiven.copy()
         newroutes = []
+        loo = 1
         for route in tryingroutes:
             noimprovement = False
             eligible = len(route.sequenceOfNodes)
-            existing_route = route
+            existing_route = copy.deepcopy(route)
             while noimprovement is False:
                 existing_route_time = self.calculateRouteTime(existing_route)
                 toBeBroken = False
@@ -287,9 +339,10 @@ class Solver:
                     if toBeBroken is True:
                         break
                     for k in range(i + 1, eligible + 1):
-                        new_route = self.twoOptSwap(existing_route, i, k)
+                        new_route = self.twoOptSwap(copy.deepcopy(existing_route), i, k)
                         new_route_time = self.calculateRouteTime(new_route)
                         if (new_route_time < existing_route_time):
+                            #print("it happened")
                             existing_route = new_route
                             existing_route_time = new_route_time
                             existing_route.time = self.calculateRouteTime(new_route)
@@ -298,15 +351,13 @@ class Solver:
                             break
                         else:
                             noimprovement = True
-            '''
-            print("route")
-            for nodes in existing_route.sequenceOfNodes:
-                print(nodes.id)
-            print(existing_route.time, existing_route_time)
-            '''
+            #print("2 opt route", loo)
+            #for nodes in existing_route.sequenceOfNodes:
+                #print(nodes.id)
+            #print(existing_route.time, existing_route_time)
+            loo += 1
             newroutes.append(existing_route)
         return newroutes
-
 
 
         """
@@ -332,10 +383,61 @@ class Solver:
 m = Model()
 m.BuildModel()
 s = Solver(m)
-sol = s.sweepMethod()
-new_sol = s.twoOpt()
+sol = Solution()
+sol.routes = s.sweepMethod()
 
+"""
+i = 1
+for route in sol.routes:
+    print("route", i, "   ", route.load, route.time)
+    for node in route.sequenceOfNodes:
+        print(node.id)
+    i += 1
+"""
 
+timesold = []
+for route in sol.routes:
+    timesold.append(s.calculateRouteTime(route))
+    #print(route.time)
+sol.cost = max(timesold)
+print(sol.cost)
+
+new_sol = Solution()
+new_sol.routes = s.twoOpt(sol.routes)
+rl_sol = Solution()
+rl_sol.routes = s.relocateFromMax(copy.deepcopy(new_sol.routes))
+
+times = []
+for newroute in new_sol.routes:
+    #for nodes in newroute.sequenceOfNodes:
+        #print(nodes.id)
+    #print("route time", route.time)
+    #print(s.calculateRouteTime(route))
+    tim = newroute.time
+    #print("time new route", tim)
+    times.append(tim)
+new_sol.cost = max(times)
+print(new_sol.cost)
+
+"""
+i = 1
+for route in new_sol.routes:
+    print("route", i, "   ", route.load, route.time)
+    for node in route.sequenceOfNodes:
+        print(node.id)
+    i +=1
+"""
+rltimes = []
+for route in rl_sol.routes:
+    rltimes.append(s.calculateRouteTime(route))
+    #print(route.time)
+rl_sol.cost = max(rltimes)
+print(rl_sol.cost)
+
+drawer = SolDrawer()
+drawer.draw("fig1", sol, m.all_nodes)
+drawer.draw("fig2", new_sol, m.all_nodes)
+drawer.draw("fig 3", rl_sol, m.all_nodes)
 
 
 
